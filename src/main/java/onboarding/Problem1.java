8a3133b2d1679c5a71a85d8d49aab13f2512a8e0
yongjunlee <frank.virus0325@gmail.com>
2022-10-27 15:51:41 -0400

    [java-onboarding]<Problem 1> 데이터 파싱, List와 Hashmap 사용

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,8 @@
 package onboarding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /*
     기능 구현 목록
@@ -12,14 +14,24 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
 
         int answer = Integer.MAX_VALUE;
-        //각 캐릭터들이 받음 점수 리스트.
+
+        List<HashMap<String, List<Integer>>> nameAndPageList = new ArrayList<>();
+        HashMap<String, List<Integer>> pobiPageInfo = new HashMap<>();
+        HashMap<String, List<Integer>> crongPageInfo = new HashMap<>();
+        pobiPageInfo.put("pobi", pobi);
+        crongPageInfo.put("crong", crong);
+        nameAndPageList.add(pobiPageInfo);
+        nameAndPageList.add(crongPageInfo);
+
+        int pobiFinalScore;
+        int crongFinalScore;
 
 
 
         return answer;
     }
 
-    public static int addNums(int pageNum) {
+    public static int addNum(int pageNum) {
         List<Integer> placeValue = new ArrayList<Integer>() {};
 
         int tempPageNum = pageNum;
@@ -50,4 +62,8 @@ class Problem1 {
         }
         return totalMultiplyNum;
     }
+
+
+
+
 }
