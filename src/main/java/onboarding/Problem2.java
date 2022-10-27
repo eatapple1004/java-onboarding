7c6fb9bb9c388550006d1e56e1d3dd2e8d18b6e7
yongjunlee <frank.virus0325@gmail.com>
2022-10-27 19:47:57 -0400

    [java-onboarding]<Problem 2> input 예외처리 함수 추가, 문자열 길이 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,23 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        try {
+            checkInputData(cryptogram);
+        }
+        catch(ArithmeticException e) {
+            return e.getMessage();
+        }
+
         String answer = "answer";
+
         return answer;
     }
+
+    public static void checkInputData(String inputString){
+        if(inputString.length() > 1000 || inputString.length() < 1) {
+            throw new ArithmeticException("length of string is over 1000 or less then 1");
+        }
+    }
+
 }
