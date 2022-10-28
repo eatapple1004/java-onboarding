537f8b6f9bcf1eda1d5afa9720a44e971902c6a4
yongjunlee <frank.virus0325@gmail.com>
2022-10-28 00:02:11 -0400

    [java-onboarding]<Problem 3> 한 숫자에서의 박수 갯수 계산 함수 추가,  calClapNum(int num)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -24,4 +24,16 @@ public class Problem3 {
         return false;
     }
 
+    public static int calClapNum(int num) {
+        int clapNum = 0;
+        int tempNum = num;
+        while(tempNum != 0) {
+            int placeValue = tempNum % 10;
+            tempNum = tempNum/10;
+            if(placeValue == 3 || placeValue == 6 || placeValue == 9) {
+                clapNum++;
+            }
+        }
+        return clapNum;
+    }
 }
