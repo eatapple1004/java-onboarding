1d5db4266faee67064453e5c557415f4b5032474
yongjunlee <frank.virus0325@gmail.com>
2022-10-29 17:15:33 -0400

    [java-onboarding]<Problem 4> 반대되는 아스키코드로 변환 기능 구현, convertASCII

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -18,7 +18,7 @@ public class Problem4 {
     }
 
 
-    public static List<Integer> Char2ASCII(List<Character> wordCharList) {
+    public static List<Integer> char2ASCII(List<Character> wordCharList) {
         List<Integer> wordASCIIList = new ArrayList<Integer>();
         for(char c : wordCharList) {
             int charASCII = (byte)c;
@@ -27,4 +27,21 @@ public class Problem4 {
         return wordASCIIList;
     }
 
+    public static List<Integer> convertASCII(List<Integer> wordASCIIList) {
+        List<Integer> convertedASCIIList = new ArrayList<Integer>();
+        for(int ascii : wordASCIIList) {
+            if(ascii >= 65 && ascii <= 90) {
+                int convertedASCII = 155 - ascii;
+                convertedASCIIList.add(convertedASCII);
+            }
+            else if(ascii >= 97 && ascii <= 122) {
+                int convertedASCII = 219 - ascii;
+                convertedASCIIList.add(convertedASCII);
+            }
+            else {
+                convertedASCIIList.add(ascii);
+            }
+        }
+        return convertedASCIIList;
+    }
 }
