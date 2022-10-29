2b10d8ad007ae8eacd104d9db71f39384ab30bc9
yongjunlee <frank.virus0325@gmail.com>
2022-10-29 16:45:42 -0400

    [java-onboarding]<Problem 4> String 형태 Char Array 형태로 변환 기능 구현, string2Char

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    public static List<Character> string2Char(String word) {
+        List<Character> wordCharList = new ArrayList<Character>();
+        for(char c : word.toCharArray()){
+            wordCharList.add(c);
+        }
+        return wordCharList;
+    }
+
 }
