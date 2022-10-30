75128068299817c966ef464952b94f1cfb36a8ff
yongjunlee <frank.virus0325@gmail.com>
2022-10-30 10:24:50 -0400

    [java-onboarding]<Problem 5> 프로세스 구현, getAnswer

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,12 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
 
-    int[] moneyUnit = {50000,10000,5000,1000,500,100,50,10,1};
+    static final int[] moneyUnits = {50000,10000,5000,1000,500,100,50,10,1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
@@ -22,4 +23,13 @@ public class Problem5 {
         return remainer;
     }
 
+    public static List<Integer> getAnswer(int money) {
+        List<Integer> answer = new ArrayList<Integer>();
+        int leftMoney = money;
+        for(int moneyUnit : moneyUnits) {
+            answer.add(getQuotient(leftMoney, moneyUnit));
+            leftMoney = getRemainder(leftMoney, moneyUnit);
+        }
+        return answer;
+    }
 }
