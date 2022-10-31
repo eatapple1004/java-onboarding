6e51befaceae7ddc9d0cdfec97ebcfcb08bc58e1
yongjunlee <frank.virus0325@gmail.com>
2022-10-30 22:07:38 -0400

    [java-onboarding]<Problem 7> 친구 리스트 작성 함수 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,13 +5,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    List<String> friendList = new ArrayList<String>();
-    List<String> relateFriendList = new ArrayList<String>();
-    List<String> visitor = new ArrayList<String>();
-    List<String> recommendFriendList = new ArrayList<String>();
+    static List<String> friendList = new ArrayList<String>();
+    static List<String> relateFriendList = new ArrayList<String>();
+    static List<String> visitorList = new ArrayList<String>();
+    static List<String> recommendFriendList = new ArrayList<String>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    public static void setFriendList(List<List<String>> friends) {
+        for(List<String> friendInfo : friends) {
+            friendList.add(friendInfo.get(0));
+        }
+    }
+
+
 
 }
