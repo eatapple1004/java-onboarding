f6e2175473503dbef307ddfcb2277bae7cdafe18
yongjunlee <frank.virus0325@gmail.com>
2022-10-30 22:04:20 -0400

    [java-onboarding]<Problem 7> 전역 변수 생성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,17 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    List<String> friendList = new ArrayList<String>();
+    List<String> relateFriendList = new ArrayList<String>();
+    List<String> visitor = new ArrayList<String>();
+    List<String> recommendFriendList = new ArrayList<String>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
 }
